bff96d749f611389bad01ea052e3f4d91552c1fe
bobking256 <bobking256@gmail.com>
2021-09-10 11:05:07 -0400

    clean up users list

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -10,19 +10,19 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <table class="table table-auto px-8 py-4">
                     <thead>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>eMail</th>
-                        <th>Pick 5-3-1</th>
-                        <th>Pick All</th>
-                        <th>Admin</th>
+                        <th class="px-2 py-1">Id</th>
+                        <th class="px-2 py-1">Name</th>
+                        <th class="px-2 py-1">eMail</th>
+                        <th class="px-2 py-1">Pick 5-3-1</th>
+                        <th class="px-2 py-1">Pick All</th>
+                        <th class="px-2 py-1">Admin</th>
                     </thead>
                     <tbody>
                         @foreach($users as $u)
                         <tr>
-                            <th class="px-2 py-1">{{ $u->id }}</th>
-                            <th class="px-2 py-1">{{ $u->name }}</th>
-                            <th class="px-2 py-1">{{ $u->email }}</th>
+                            <td class="px-2 py-1">{{ $u->id }}</td>
+                            <td class="px-2 py-1">{{ $u->name }}</td>
+                            <td class="px-2 py-1">{{ $u->email }}</td>
                             <th class="px-2 py-1"><input disabled type="checkbox" {{ $u->pick531 ? 'checked' : '' }}></th>
                             <th class="px-2 py-1"><input disabled type="checkbox" {{ $u->pickall ? 'checked' : '' }}></th>
                             <th class="px-2 py-1"><input disabled type="checkbox" {{ $u->admin ? 'checked' : '' }}></th>
