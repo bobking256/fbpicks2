51095e7cde84163281d27c6025cabc693701d3ce
bobking256 <bobking256@gmail.com>
2021-09-12 21:50:37 -0400

    update not picked all

--- resources/views/pickall/notpickall.blade.php
+++ resources/views/pickall/notpickall.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Pick 5-3-1') }}
+            {{ __('Not Picked All') }}
         </h2>
     </x-slot>
 
