1763d4091403cbd56921d77466db7aabb2d17ea1
bobking256 <bobking256@gmail.com>
2021-09-23 19:40:17 -0400

    formatting

--- resources/views/pickall/pickslocked.blade.php
+++ resources/views/pickall/pickslocked.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 sm:text-sm md:text-sm">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-6 py-4">
                 <table class="table table-auto">
                     <thead>
