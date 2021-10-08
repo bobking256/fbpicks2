c28ff496295639202e01fcc2afb912c174693341
bobking256 <bobking256@gmail.com>
2021-10-08 11:50:41 -0400

    fixed bonus

--- app/Http/Traits/Pick531Trait.php
+++ app/Http/Traits/Pick531Trait.php
@@ -7,173 +7,182 @@ use App\Models\User;
 use App\Models\Result;
 use Illuminate\Support\Facades\Log;
 
-trait Pick531Trait {
+trait Pick531Trait
+{
     use SupportTrait;
 
 
-	public function getNotPicked531(){
+    public function getNotPicked531()
+    {
         $week_no = $this->getCurrentWeek();
-		$users = $this->getUsers531();
-//		$picked = $this->Pick->find('all',array('conditions'=>array('Pick.week_no'=>$week_no,'Pick.def'=>0)),array('user_id'));
+        $users = $this->getUsers531();
+        //		$picked = $this->Pick->find('all',array('conditions'=>array('Pick.week_no'=>$week_no,'Pick.def'=>0)),array('user_id'));
 
-		$picked = Pick::where('week_no',$week_no)
-			->where('def',0)
-			->get()->toArray();
+        $picked = Pick::where('week_no', $week_no)
+            ->where('def', 0)
+            ->get()->toArray();
 
-		$count=0;
-		$notpicked=array();
+        $count = 0;
+        $notpicked = array();
         Log::debug('Not picked empty');
         Log::debug($notpicked);
-		for($i=0;$i<sizeof($users);$i++){
-				$found=0;
-				$uid = $users[$i]['id'];
-				for($k=0;$k<sizeof($picked);$k++){
-					$puid = $picked[$k]['user_id'];
-					if($puid == $uid) { $found=1; break; }
-				}
-				if($found == 0) {
-                    $notpicked[$count] = [];
-					$notpicked[$count]['name'] = $users[$i]['name'];
-					$notpicked[$count]['id'] = $users[$i]['id'];
-					$notpicked[$count]['email'] = $users[$i]['email'];
-					$count++;
-				}
-		}
-		return $notpicked;
-	}
+        for ($i = 0; $i < sizeof($users); $i++) {
+            $found = 0;
+            $uid = $users[$i]['id'];
+            for ($k = 0; $k < sizeof($picked); $k++) {
+                $puid = $picked[$k]['user_id'];
+                if ($puid == $uid) {
+                    $found = 1;
+                    break;
+                }
+            }
+            if ($found == 0) {
+                $notpicked[$count] = [];
+                $notpicked[$count]['name'] = $users[$i]['name'];
+                $notpicked[$count]['id'] = $users[$i]['id'];
+                $notpicked[$count]['email'] = $users[$i]['email'];
+                $count++;
+            }
+        }
+        return $notpicked;
+    }
 
-	public function getUsers531Count()
-	{
-		$users = $this->getPickUsers();
-		return sizeof($users);
-   	}
+    public function getUsers531Count()
+    {
+        $users = $this->getPickUsers();
+        return sizeof($users);
+    }
 
     public function getUsers531()
     {
-		$users = User::where('pick531',1)->get()->toArray();
+        $users = User::where('pick531', 1)->get()->toArray();
 
         return $users;
-   	}
+    }
 
 
 
 
-	public function setDefaultPicks531($id,$weekno,$p5,$p3,$p1)
-	{
+    public function setDefaultPicks531($id, $weekno, $p5, $p3, $p1)
+    {
 
-		$pick = Pick::where('user_id',$id)
-			->where('week_no',$weekno)
-			->first();
-		if($pick != null){
-			$pick->delete();
-		}
+        $pick = Pick::where('user_id', $id)
+            ->where('week_no', $weekno)
+            ->first();
+        if ($pick != null) {
+            $pick->delete();
+        }
 
 
-		$def_pick = new Pick;
+        $def_pick = new Pick;
 
-		$def_pick->user_id = $id;
-		$def_pick->week_no = $weekno;
-		$def_pick->pt5 = $p5;
-		$def_pick->pt3 = $p3;
-		$def_pick->pt1 = $p1;
-		$def_pick->def = true;
+        $def_pick->user_id = $id;
+        $def_pick->week_no = $weekno;
+        $def_pick->pt5 = $p5;
+        $def_pick->pt3 = $p3;
+        $def_pick->pt1 = $p1;
+        $def_pick->def = true;
         $def_pick->bonus = 0;
 
         $def_pick->save();
-	}
+    }
 
 
-	public function deletedefaults531($weekno=0){
-		if($weekno==0) return;
+    public function deletedefaults531($weekno = 0)
+    {
+        if ($weekno == 0) return;
 
-		$defpicks = Pick::where('def',1)->where('week_no',$weekno)->get(['id']);
+        $defpicks = Pick::where('def', 1)->where('week_no', $weekno)->get(['id']);
         Pick::destroy($defpicks);
-
-	}
-
-
-	public function getpicks531($user,$week){
-		return Pick::where('user_id',$user)
-			->where('week_no',$week)
-			->first();
-
-	}
+    }
 
 
-	public function processResults531($week_no){
-		$scheds = $this->getSchedule($week_no);
-
-        $weekres=array();
-		$weekres = $this->getWeekResults($week_no);
-
-		$users = $this->getUsers531();
-
-        for($i=0;$i<sizeof($users);$i++){
-			$usr_picks = $this->getpicks531($users[$i]['id'],$week_no);
-//			$usr_picks = request()Action('/picks/getpick531/'.$users[$i]['User']['id'].'/'.$week_no);
+    public function getpicks531($user, $week)
+    {
+        return Pick::where('user_id', $user)
+            ->where('week_no', $week)
+            ->first();
+    }
 
 
-			$usr_res['user_id'] = $users[$i]['id'];
-			$usr_res['week_no'] = $week_no;
+    public function processResults531($week_no)
+    {
+        $scheds = $this->getSchedule($week_no);
 
-			if($usr_picks['def'] == 1){
-				$pts[0]=3; $pts[1]=2; $pts[2]=1; $pts[3]=0;
-			} else {
-				$pts[0]=5; $pts[1]=3; $pts[2]=1; $pts[3]=5;
-			}
+        $weekres = array();
+        $weekres = $this->getWeekResults($week_no);
+        $users = $this->getUsers531();
 
-			$usr_res['pt5'] = 0;
-			for($k=0;$k<sizeof($weekres);$k++){
-				if($usr_picks['pt5'] == $weekres[$k]){
-					$usr_res['pt5'] = $pts[0];
-					break;
-				}
-			}
+        for ($i = 0; $i < sizeof($users); $i++) {
+            $usr_picks = $this->getpicks531($users[$i]['id'], $week_no);
+            //			$usr_picks = request()Action('/picks/getpick531/'.$users[$i]['User']['id'].'/'.$week_no);
+            Log::debug($usr_picks);
 
-			$usr_res['pt3'] = 0;
-			for($k=0;$k<sizeof($weekres);$k++){
-				if($usr_picks['pt3'] == $weekres[$k]){
-					$usr_res['pt3'] = $pts[1];
-					break;
-				}
-			}
+            $usr_res['user_id'] = $users[$i]['id'];
+            $usr_res['week_no'] = $week_no;
 
-			$usr_res['pt1'] = 0;
-			for($k=0;$k<sizeof($weekres);$k++){
-				if($usr_picks['pt1'] == $weekres[$k]){
-					$usr_res['pt1'] = $pts[2];
-					break;
-				}
-			}
+            if ($usr_picks['def'] == 1) {
+                $pts[0] = 3;
+                $pts[1] = 2;
+                $pts[2] = 1;
+                $pts[3] = 0;
+            } else {
+                $pts[0] = 5;
+                $pts[1] = 3;
+                $pts[2] = 1;
+                $pts[3] = 5;
+            }
 
-			$usr_res['bonus'] = 0;
-			for($k=0;$k<sizeof($weekres);$k++){
-				if($usr_picks['bonus'] != 0 && $usr_picks['Pick']['bonus'] == $weekres[$k]){
-					$usr_res['bonus'] = $pts[3];
-					break;
-				}
-			}
+            $usr_res['pt5'] = 0;
+            for ($k = 0; $k < sizeof($weekres); $k++) {
+                if ($usr_picks['pt5'] == $weekres[$k]) {
+                    $usr_res['pt5'] = $pts[0];
+                    break;
+                }
+            }
 
-			$usr_res['week_no'] = $week_no;
-			$usr_res['user_id'] = $users[$i]['id'];
+            $usr_res['pt3'] = 0;
+            for ($k = 0; $k < sizeof($weekres); $k++) {
+                if ($usr_picks['pt3'] == $weekres[$k]) {
+                    $usr_res['pt3'] = $pts[1];
+                    break;
+                }
+            }
 
-			$r = $this->getResultByUser($users[$i]['id'],$week_no);
+            $usr_res['pt1'] = 0;
+            for ($k = 0; $k < sizeof($weekres); $k++) {
+                if ($usr_picks['pt1'] == $weekres[$k]) {
+                    $usr_res['pt1'] = $pts[2];
+                    break;
+                }
+            }
 
-			if($r == null){
-				$sql = Result::create($usr_res);
-			} else {
-				$sql = Result::find($r['id']);
+            $usr_res['bonus'] = 0;
+            for ($k = 0; $k < sizeof($weekres); $k++) {
+                if ($usr_picks['bonus'] != 0 && $usr_picks['bonus'] == $weekres[$k]) {
+                    $usr_res['bonus'] = $pts[3];
+                    break;
+                }
+            }
+
+            $usr_res['week_no'] = $week_no;
+            $usr_res['user_id'] = $users[$i]['id'];
+
+            $r = $this->getResultByUser($users[$i]['id'], $week_no);
+
+            if ($r == null) {
+                $sql = Result::create($usr_res);
+            } else {
+                $sql = Result::find($r['id']);
                 $sql->update($usr_res);
-			}
-		}
-	}
+            }
+        }
+    }
 
 
-	public function delete531($user_id)
-	{
-        $picks = Pick::where('user_id',$user_id)->get(['id']);
+    public function delete531($user_id)
+    {
+        $picks = Pick::where('user_id', $user_id)->get(['id']);
         Pick::destroy($picks);
-	}
-
-
+    }
 }
